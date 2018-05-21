b19e503aa00952e3307e17ac8ff2eecaa9ab94f6
Matthis Hagen <matthis.hagen@o2online.de>
2018-05-21 12:57:21 +0200

    - hopefully fixed WeaponBug

--- Assets/Scripts/Utilities/MatchManager.cs
+++ Assets/Scripts/Utilities/MatchManager.cs
@@ -104,7 +104,7 @@ public class MatchManager : MonoBehaviour  {
         // Reset PlayerScripts
         player.GetComponent<Health>().ResetHP();
         player.GetComponent<Hero_Movement>().antVisual.SetMovePercent(0);
-        player.GetComponentInChildren<Hero_Wpn_Controller>().switchToWeapon(playerPrefab.GetComponentInChildren<Hero_Wpn_Controller>().GetComponentInChildren<Hero_Wpn_Info>());
+        player.GetComponentInChildren<Hero_Wpn_Controller>().switchToWeapon(player.GetComponentInChildren<Hero_Wpn_Controller>().GetComponentInChildren<Hero_Wpn_Info>());
     }
 
     public void InitPlayer(int PlayerID)
